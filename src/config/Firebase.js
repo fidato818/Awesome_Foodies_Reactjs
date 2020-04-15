8fff11c6ca62c31be13387d980369b46c9f0f0df
Adnan Ahmed <adnanahmed818@gmail.com>
2020-04-16 01:22:02 +0500

    Update Commit

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBAFV0T0p1OzAJ0AvSChnIPraetH4fzKjQ",
-  authDomain: "awsomefoodies.firebaseapp.com",
-  databaseURL: "https://awsomefoodies.firebaseio.com",
-  projectId: "awsomefoodies",
-  storageBucket: "awsomefoodies.appspot.com",
-  messagingSenderId: "252615936927",
-  appId: "1:252615936927:web:16a65d648df9667d5b0baa",
-  measurementId: "G-M0PKH2RF6E"
+  apiKey: "***************************************",
+  authDomain: "***************************************",
+  databaseURL: "***************************************",
+  projectId: "***************************************",
+  storageBucket: "***************************************",
+  messagingSenderId: "***************************************",
+  appId: "***************************************",
+  measurementId: "***************************************"
 };
 
 // Initialize Firebase
